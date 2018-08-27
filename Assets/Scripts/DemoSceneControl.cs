a8696672cfa513fb42181a58ff983edd575fbfe3
Tonci Jukic <tonci@flyingpenguin.tech>
2018-08-28 00:47:26 +0200

    Project setup changes, small additions. Back button handler for the main demo scene. Some UI graphics. Icons, project names, project titles.

---
+++ Assets/Scripts/DemoSceneControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DemoSceneControl : MonoBehaviour {
+
+    int sceneIdx;
+
+    // Use this for initialization
+    void Start()
+    {
+        sceneIdx = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Escape) && sceneIdx > 0)
+        //    SceneManager.LoadScene(sceneIdx - 1);
+        //else Application.Quit();
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+
+    }
+}
